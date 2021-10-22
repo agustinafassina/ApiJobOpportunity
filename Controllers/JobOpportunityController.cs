f7eee7440eca7f35d5222f13fd51c439f231f84a
Agustina Fassina <agustinafassina@gmail.com>
2021-10-22 17:33:05 -0300

    first commit

---
+++ Controllers/JobOpportunityController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using ApiJobOpportunity.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiJobOpportunity.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class JobOpportunityController : ControllerBase
+    {
+        private readonly IJobOpportunityService _jobOpportunityService;
+
+        public JobOpportunityController(IJobOpportunityService jobOpportunityService)
+        {
+            _jobOpportunityService = jobOpportunityService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] JobOpportunity request)
+        {
+            await _jobOpportunityService.Post(request);
+            return Ok();
+        }
+
+        [HttpGet]
+        public object Get([FromQuery] JobOpportunity filters)
+        {
+            return _jobOpportunityService.JobSearch(filters);
+        }
+
+        [HttpPost("suscriptions")]
+        public async Task<IActionResult> PostSuscription([FromBody] User request)
+        {
+            await _jobOpportunityService.PostSuscription(request);
+            return Ok();
+        }
+
+        [HttpGet("suscriptions/user")]
+        public object GetSuscriptionUser()
+        {
+            return _jobOpportunityService.GetSuscriptionUser();
+        }
+    }
+}
